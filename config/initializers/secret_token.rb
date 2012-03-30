99645bb743b01cf0d78e01f31df47fabc8d853f0
Matthew Smith <matt@nearapogee.com>
2012-03-30 09:57:50 -0700

    initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PumaCache::Application.config.secret_token = '9a1dbc2856a21576e78c7449380659126173842aa5c25d5f3713b73134f618fae7c8291e1adcc4e15a37af60cbe1294eb9dd24c7aa5631dcb4a1206a378722a1'
